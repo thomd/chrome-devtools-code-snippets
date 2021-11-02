c5c7ce7572eb1a9d0128b66a22a30367790fb1c6
Thomas Dürr <thomduerr@gmail.com>
2021-11-02 23:15:21 +0100

    ct.css

---
+++ ct.css.js
@@ -0,0 +1,9 @@
+// source: https://github.com/csswizardry/ct
+(function(){
+  var ct = document.createElement('link')
+  ct.rel = 'stylesheet'
+  ct.href = 'https://csswizardry.com/ct/ct.css'
+  ct.classList.add('ct')
+  document.head.appendChild(ct)
+}())
+
